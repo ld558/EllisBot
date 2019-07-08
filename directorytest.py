192b2d43690a0f89578551922fa8374139b9eacb
ld558 <45179374+ld558@users.noreply.github.com>
2019-07-08 14:48:30 +0100

    Update directorytest.py

--- directorytest.py
+++ directorytest.py
@@ -3,3 +3,5 @@ import os
 # Print current working directory 
 print "Current working dir : %s" % os.getcwd()
 with open("/vagrant/home/EllisBot/reddit_usernames_replied_to.txt","r") as f:
+   f.write("hello")
+
