31a929b3079f9e1a89a1d68438e24913b32abfc5
ld558 <45179374+ld558@users.noreply.github.com>
2019-07-08 14:50:25 +0100

    Update directorytest.py

--- directorytest.py
+++ directorytest.py
@@ -2,6 +2,6 @@ import os
 
 # Print current working directory 
 print "Current working dir : %s" % os.getcwd()
-with open("/vagrant/home/EllisBot/reddit_usernames_replied_to.txt","r") as f:
+with open("/vagrant/home/EllisBot/reddit_usernames_replied_to.txt","w") as f:
    f.write("hello")
 
